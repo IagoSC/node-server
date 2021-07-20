d1c7f46ce65ef868922abfc0376ccf6618217af1
iago de sousa <iago.sousa@leanonsystems.com>
2021-07-20 12:38:26 -0300

    basic SOLID w/ singleton

--- src/routes/specification.routes.ts
+++ src/routes/specification.routes.ts
@@ -1,25 +1,9 @@
 import { Router } from "express";
 
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 export const specificationRoutes = Router();
 
-const specificationRepository = new SpecificationsRepository();
-
 specificationRoutes.post("/", (req, res) => {
-  const { name, description } = req.body;
-
-  const createSpecificationService = new CreateSpecificationService(
-    specificationRepository
-  );
-
-  createSpecificationService.execute({ name, description });
-
-  res.status(201).send();
-});
-
-specificationRoutes.get("/", (req, res) => {
-  const all = specificationRepository.getAll();
-  res.status(200).send(all);
+  return createSpecificationController.handle(req, res);
 });
